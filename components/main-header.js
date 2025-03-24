1eb0fb8d40af7369473b85f664d914f6037390a2
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-03-24 14:31:43 +0800

    Add MainHeader component with navigation links and update layout

---
+++ components/main-header.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+import logoImg from "@/assets/logo.png";
+
+import styles from "./main-header.module.css";
+
+export default function MainHeader() {
+  return (
+    <header className={styles.header}>
+      <Link className={styles.logo} href="/">
+        <img src={logoImg.src} alt="A plate with food in it" />
+        NextLevel Food
+      </Link>
+
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
